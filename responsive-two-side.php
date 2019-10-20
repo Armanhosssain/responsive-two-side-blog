e53f904fbf6c84afd8f2774e561f1c5136b124cf
Arman hossain <armanh688@gmail.com>
2019-10-21 01:06:22 +0600

    two side blog

---
+++ responsive-two-side.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/uikit.min.css">
+  <link rel="shortcut icon" type="image/x-icon" href="img/a.png">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/style.css">
+	<title>responsive two side </title>
+</head>
+<body>
+ <!-- start home info section -->
+  <section id="info" class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-md-6 align-self-center">
+          <h3>Lorem Ipsum Dolor Sit</h3>
+          <p class="lead">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium provident, laudantium possimus voluptatum incidunt non eos aut in saepe tenetur vero ad eligendi maxime vitae voluptatibus hic quisquam, veniam pariatur.
+          </p>
+          <a href="#" class="btn btn-outline-dark">Read More</a>
+        </div>
+        <div class="col-lg-6 col-md-6">
+          <img src="img/info-right-img.jpg" alt="info image" class="img-fluid rounded">
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end home info section -->
+   <script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/uikit.min.js"></script>
+  <script src="js/uikit-icons.min.js"></script>
+</body>
+</html>
